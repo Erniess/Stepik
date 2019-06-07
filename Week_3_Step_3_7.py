fa33bf50ae99009426da33af4fa87bd04553b8f5
Erniess <dragon_a@rambler.ru>
2019-06-07 11:17:39 +0300

    Regexp training

---
+++ Week_3_Step_3_7.py
@@ -0,0 +1,26 @@
+#
+# Вам дана последовательность строк.
+# В каждой строке замените первое вхождение слова, состоящего только из латинских букв "a" (регистр не важен), на слово "argh".
+#
+# Примечание:
+# Обратите внимание на параметр count у функции sub.
+#
+# Sample Input:
+#
+# There’ll be no more "Aaaaaaaaaaaaaaa"
+# AaAaAaA AaAaAaA
+#
+# Sample Output:
+#
+# There’ll be no more "argh"
+# argh AaAaAaA
+#
+
+import re
+import sys
+pattern = r'[aA]+'
+
+for line in sys.stdin:
+    line = line.rstrip()
+    if re.findall(pattern, line): line = re.sub(pattern, r'argh', line, 1)
+    print(line)
