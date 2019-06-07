fdc8c64134aca355e4d377e46553e41401960800
Erniess <dragon_a@rambler.ru>
2019-06-07 09:56:30 +0300

    Regexp training

--- Week_3_Step_3_1.py
+++ Week_3_Step_3_1.py
@@ -1,6 +1,21 @@
 # Вам дана последовательность строк.
 # Выведите строки, содержащие "cat" в качестве подстроки хотя бы два раза.
 
+#
+# Sample Input:
+#
+# catcat
+# cat and cat
+# catac
+# cat
+# ccaatt
+#
+# Sample Output:
+#
+# catcat
+# cat and cat
+#
+
 import re
 import sys
 pattern = r'cat.*cat'
--- Week_3_Step_3_2.py
+++ Week_3_Step_3_2.py
@@ -1,5 +1,24 @@
 # Вам дана последовательность строк.
 # Выведите строки, содержащие "cat" в качестве слова.
+#
+#
+# Sample Input:
+#
+# cat
+# catapult and cat
+# catcat
+# concat
+# Cat
+# "cat"
+# !cat?
+#
+# Sample Output:
+#
+# cat
+# catapult and cat
+# "cat"
+# !cat?
+#
 
 import re
 import sys
