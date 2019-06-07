d8c8a839e0743749aea79b5644da4a9aa8ca5936
Erniess <dragon_a@rambler.ru>
2019-06-07 10:55:45 +0300

    Regexp training

--- Week_3_Step_3_3.py
+++ Week_3_Step_3_3.py
@@ -1,5 +1,21 @@
 # Вам дана последовательность строк.
 # Выведите строки, содержащие две буквы "z﻿", между которыми ровно три символа.
+#
+#
+# Sample Input:
+#
+# zabcz
+# zzz
+# zzxzz
+# zz
+# zxz
+# zzxzxxz
+#
+# Sample Output:
+#
+# zabcz
+# zzxzz
+#
 
 import re
 import sys
--- Week_3_Step_3_4.py
+++ Week_3_Step_3_4.py
@@ -1,6 +1,17 @@
 # Вам дана последовательность строк.
 # Выведите строки, содержащие обратный слеш "\﻿".
 
+#
+# Sample Input:
+#
+# \w denotes word character
+# No slashes here
+#
+# Sample Output:
+#
+# \w denotes word character
+#
+
 import re
 import sys
 pattern = r'\\.*'
