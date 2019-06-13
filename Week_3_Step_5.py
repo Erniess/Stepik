25b4e6f06cad2f7647bf06c165e2340e87f94187
Erniess <dragon_a@rambler.ru>
2019-06-13 17:33:11 +0300

    http, html and requests training

--- Week_3_Step_5.py
+++ Week_3_Step_5.py
@@ -23,3 +23,33 @@ with open('example.csv', 'a') as f:
 with open('example.csv', 'a') as f:
     writer = csv.writer(f, quoting=csv.QUOTE_ALL) # явно указать все помещать в кавычки
     writer.writerows(students)
+
+# JSON
+import json
+
+student1 = {
+    "first_name": "Greg",
+    "last_name": "Dean",
+    "certificate": True,
+    "scores": [
+      70,
+      80,
+      90
+    ],
+    "description": "Good job, Greg"
+}
+student2 = {
+    "first_name": "Wirt",
+    "last_name": "Wood",
+    "certificate": True,
+    "scores": [
+      70,
+      80,
+      90
+    ],
+    "description": "Nicely Day"
+}
+data = [student1, student2]
+#print(json.dumps(data, indent=4, sort_keys=True)) # Объект, количество отступов, сортировка
+with open('example.json', 'w') as f:
+    json.dump(data, f, indent=4, sort_keys=True) # Запись в файл
