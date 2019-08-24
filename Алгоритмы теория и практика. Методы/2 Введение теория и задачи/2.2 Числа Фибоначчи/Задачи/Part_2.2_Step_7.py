95325e12ce4c178ad9fad4a2978aab7f11474424
Erniess <dragon_a@rambler.ru>
2019-08-24 19:09:35 +0300

    Algorithms Fibonacci task 8

--- Алгоритмы теория и практика. Методы/2 Введение теория и задачи/2.2 Числа Фибоначчи/Задачи/Part_2.2_Step_7.py
+++ Алгоритмы теория и практика. Методы/2 Введение теория и задачи/2.2 Числа Фибоначчи/Задачи/Part_2.2_Step_7.py
@@ -2,10 +2,9 @@
 
 def fib_digit(n):
     if n == 1 or n == 2: return 1
-    a, b = 1, 1
-    for _ in range(2, n):
-        b = (a + b) % 10
-        a = b - a
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, (a + b) % 10
     return b
 
 def main():
